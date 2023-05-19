b2bc55c25a8cc6fd5e14e16b439bd28d42f754d7
vincenthou <vincenthou365@gmail.com>
2023-05-19 21:43:48 +0800

    fix(migrate): update migrate script

--- app/prisma/migrations/20221210115719_first/migration.sql
+++ app/prisma/migrations/20221210115719_first/migration.sql
@@ -1,90 +1,106 @@
--- CreateTable
-CREATE TABLE `User` (
-    `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NULL,
-    `username` VARCHAR(191) NULL,
-    `email` VARCHAR(191) NULL,
-    `emailVerified` DATETIME(3) NULL,
-    `image` VARCHAR(191) NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
-
-    UNIQUE INDEX `User_username_key`(`username`),
-    UNIQUE INDEX `User_email_key`(`email`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 -- CreateTable
-CREATE TABLE `Account` (
-    `id` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
-    `type` VARCHAR(191) NOT NULL,
-    `provider` VARCHAR(191) NOT NULL,
-    `providerAccountId` VARCHAR(191) NOT NULL,
-    `refresh_token` VARCHAR(191) NULL,
-    `access_token` TEXT NULL,
-    `id_token` TEXT NULL,
-    `expires_at` INTEGER NULL,
-    `token_type` VARCHAR(191) NULL,
-    `scope` VARCHAR(191) NULL,
-    `session_state` VARCHAR(191) NULL,
-    `oauth_token_secret` VARCHAR(191) NULL,
-    `oauth_token` VARCHAR(191) NULL,
+CREATE TABLE "User" (
+    "id" VARCHAR(191) NOT NULL,
+    "name" VARCHAR(191) NULL,
+    "username" VARCHAR(191) NULL,
+    "email" VARCHAR(191) NULL,
+    "emailVerified" TIMESTAMP(3) NULL,
+    "image" VARCHAR(191) NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    INDEX `Account_userId_idx`(`userId`),
-    UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `Session` (
-    `id` VARCHAR(191) NOT NULL,
-    `sessionToken` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
-    `expires` DATETIME(3) NOT NULL,
+CREATE TABLE "Account" (
+    "id" VARCHAR(191) NOT NULL,
+    "userId" VARCHAR(191) NOT NULL,
+    "type" VARCHAR(191) NOT NULL,
+    "provider" VARCHAR(191) NOT NULL,
+    "providerAccountId" VARCHAR(191) NOT NULL,
+    "refresh_token" VARCHAR(191) NULL,
+    "access_token" TEXT NULL,
+    "id_token" TEXT NULL,
+    "expires_at" INTEGER NULL,
+    "token_type" VARCHAR(191) NULL,
+    "scope" VARCHAR(191) NULL,
+    "session_state" VARCHAR(191) NULL,
+    "oauth_token_secret" VARCHAR(191) NULL,
+    "oauth_token" VARCHAR(191) NULL,
 
-    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
-    INDEX `Session_userId_idx`(`userId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `VerificationToken` (
-    `identifier` VARCHAR(191) NOT NULL,
-    `token` VARCHAR(191) NOT NULL,
-    `expires` DATETIME(3) NOT NULL,
-
-    UNIQUE INDEX `VerificationToken_token_key`(`token`),
-    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `Conversation` (
-    `id` VARCHAR(191) NOT NULL,
-    `title` VARCHAR(191) NOT NULL,
-    `avatar` LONGTEXT NULL,
-    `content` JSON NULL,
-    `password` VARCHAR(191) NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
-    `creatorId` VARCHAR(191) NULL,
-    `views` INTEGER NOT NULL DEFAULT 0,
+CREATE TABLE "Session" (
+    "id" VARCHAR(191) NOT NULL,
+    "sessionToken" VARCHAR(191) NOT NULL,
+    "userId" VARCHAR(191) NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
 
-    INDEX `Conversation_creatorId_idx`(`creatorId`),
-    INDEX `Conversation_views_idx`(`views` DESC),
-    INDEX `Conversation_createdAt_idx`(`createdAt`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `Upvote` (
-    `id` VARCHAR(191) NOT NULL,
-    `conversationId` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+CREATE TABLE "VerificationToken" (
+    "identifier" VARCHAR(191) NOT NULL,
+    "token" VARCHAR(191) NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
 
-    INDEX `Upvote_conversationId_idx`(`conversationId`),
-    INDEX `Upvote_userId_idx`(`userId`),
-    UNIQUE INDEX `Upvote_conversationId_userId_key`(`conversationId`, `userId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("identifier")
+);
+
+-- CreateTable
+CREATE TABLE "Conversation" (
+    "id" VARCHAR(191) NOT NULL,
+    "title" VARCHAR(191) NOT NULL,
+    "avatar" TEXT NULL,
+    "content" JSON NULL,
+    "password" VARCHAR(191) NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "creatorId" VARCHAR(191) NULL,
+    "views" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Upvote" (
+    "id" VARCHAR(191) NOT NULL,
+    "conversationId" VARCHAR(191) NOT NULL,
+    "userId" VARCHAR(191) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    CONSTRAINT "Upvote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Account_userId_idx" ON "Account"("userId");
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE INDEX "Session_userId_idx" ON "Session"("userId");
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE INDEX "Conversation_creatorId_idx" ON "Conversation"("creatorId");
+CREATE INDEX "Conversation_views_idx" ON "Conversation"("views" DESC);
+CREATE INDEX "Conversation_createdAt_idx" ON "Conversation"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Upvote_conversationId_idx" ON "Upvote"("conversationId");
+CREATE INDEX "Upvote_userId_idx" ON "Upvote"("userId");
+CREATE UNIQUE INDEX "Upvote_conversationId_userId_key" ON "Upvote"("conversationId", "userId");
